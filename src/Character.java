c59565db138ea54ecc67673fbb68ee9f6de386cb
FAH <apichaya.inkhiewsai@gmail.com>
2023-12-07 01:33:22 +0700

    edit Character.java

--- src/Character.java
+++ src/Character.java
@@ -2,13 +2,13 @@ import java.util.List;
 public interface Character {
     /**
      * gets name of character
-     * returns: name of character
+     * @returns name of character
      */
     String getName();
 
     /**
      * gets level of character
-     * returns: level of the character
+     * @returns value of level
      */
     int getLevel();
 
@@ -22,43 +22,44 @@ public interface Character {
 
     /**
      * gets current exp of character
-     * returns: currentExp of the character
+     * @returns value of currentExp
      */
     int getCurrentExp();
 
     /**
      * gets max exp of character
-     * returns: maxExp of the character
+     * @returns value of maxExp
      */
     int getMaxExp();
 
     /**
      * gets max hp of character
-     * returns: maxHp of the character
+     * @returns value of maxHp
      */
     int getMaxHp();
 
     /**
      * gets max mana of character
-     * returns: maxMana of the character
+     * @returns value of maxMana
      */
     int getMaxMana();
 
     /**
      * gets current mana of character
-     * returns: currentMana of the character
+     * @returns value of currentMana
      */
     int getCurrentMana();
 
     /**
      * set current exp of character equal to input
+     * @param exp value of current exp of character
      * effects: assign new value of currentExp by input exp
      */
     void setCurrentExp(int exp);
 
     /**
      * gets job name of character
-     * returns: Job of character
+     * @returns Job of character
      */
     String getJob();
 
@@ -76,18 +77,21 @@ public interface Character {
 
     /**
      * increase buff attack of character by the input value
+     * @param atk value of buff attack from accessory
      * effects: buffAtk increased by atk
      */
     void setBuffAtk(double atk);
 
     /**
      * increase buff defense of character by the input value
+     * @param def value of buff def from accessory
      * effects: buffDef increased by def
      */
     void setBuffDef(double def);
 
     /**
-     * increase buff mam of character by the input value
+     * increase buff mana of character by the input value
+     * @param mana value of buff mana from accessory
      * effects: buffMana increased by mana
      */
     void setBuffMana(double mana);
@@ -101,66 +105,71 @@ public interface Character {
 
     /**
      * use to set truth value of character equipping boots
+     * @param boot truth value of equipping boots
      * effects: update truth value of isBootEquip = boot
      */
     void setBootEquip(boolean boot);
 
     /**
      * gets character is wearing boots or not
-     * returns: boolean of character is wearing boots or not
+     * @returns truth value of character is wearing boots or not
      */
     boolean getBootEquip();
 
     /**
      * use to set truth value of character equipping ring
+     * @param ring truth value of equipping ring
      * effects: update truth value of isRingEquip = ring
      */
     void setRingEquip(boolean ring);
 
     /**
      * gets character is wearing ring or not
-     * returns: boolean of character is wearing ring or not
+     * @returns truth value of character is wearing ring or not
      */
     boolean getRingEquip();
 
     /**
      * use to set truth value of character equipping armor
+     * @param armor truth value of equipping armor
      * effects: update truth value of isArmorEquip = armor
      */
     void setArmorEquip(boolean armor);
 
     /**
      * gets character is wearing armor or not
-     * returns: boolean of character is wearing armor or not
+     * @returns truth value of character is wearing armor or not
      */
     boolean getArmorEquip();
 
     /**
      * use to set consciousness of character
+     * @param unconscious truth value pf consciousness of character
      * effects: assign isUnConscious = unconscious
      */
     void setIsUnconscious(boolean unconscious);
 
     /**
      * gets character is unconscious or not
-     * returns: truth value of consciousness of character*/
+     * @returns truth value of consciousness of character*/
     boolean getIsUnconscious();
 
     /**
      * gets amount of hp that character currently has
-     * returns: currentHp of character
+     * @returns value of currentHp
      */
     int getCurrentHp();
 
     /**
      * assign current hp of character
+     * @param hp value of current hp pf character
      * effects: assign currentHp = input hp
      */
     void setCurrentHp(int hp);
 
     /**
      * gets defense of character
-     * return: def of character
+     * @returns value of def
      */
     double getDef();
 
@@ -175,7 +184,7 @@ public interface Character {
      * effects: decrease currentMana by 2 then print out mana consumption and damage output
      * effects: "You run out of mana" printed if currentMana is not enough
      * effects: "Character is unconscious" printed if character is unconscious
-     * returns: amount of damage that character does (0 damage when unconscious or run out of mana)
+     * @returns amount of damage that character does (0 damage when unconscious or run out of mana)
      */
     double attack();
 
@@ -184,7 +193,7 @@ public interface Character {
      * effects: decrease currentMana by 10 then print out mana consumption and damage output
      * effects: "You run out of mana" printed if currentMana is not enough
      * effects: "Character is unconscious" printed if character is unconscious
-     * returns: amount of damage that character does (0 damage when unconscious or run out of mana)
+     * @returns amount of damage that character does (0 damage when unconscious or run out of mana)
      */
     double skill1();
 
@@ -193,12 +202,13 @@ public interface Character {
      * effects: decrease currentMana by 30 then print out mana consumption and damage output
      * effects: "You run out of mana" printed if currentMana is not enough
      * effects: "Character is unconscious" printed if character is unconscious
-     * returns: amount of damage that character does (0 damage when unconscious or run out of mana)
+     * @returns amount of damage that character does (0 damage when unconscious or run out of mana)
      */
     double skill2();
 
     /**
      * character gaining exp
+     * @param exp value of exp that character gain
      * effects: currentExp of character increase by value of exp
      * effects: level up when currentExp is reach max*/
     default void updateExp(int exp){
@@ -211,6 +221,7 @@ public interface Character {
 
     /**
      * character put on boots accessory
+     * @param boot accessory boot type
      * effects: "you already equip boot" printed if character is already wore boot
      * effects: update buff stats and total stats of character consider to effects of boot
      */
@@ -230,6 +241,7 @@ public interface Character {
 
    /**
     * character take off boots accessory
+    * @param boot accessory boot type
     * effects: "You didn't equip boots yet" printed if character didn't wear boots
     * effects: cancel buff effects of boot from total stats of character
     */
@@ -249,6 +261,7 @@ public interface Character {
 
     /**
      * character put on ring accessory
+     * @param ring accessory ring type
      * effects: "you already equip ring" printed if character is already wore ring
      * effects: update buff stats and total stats of character consider to effects of ring
      */
@@ -267,6 +280,7 @@ public interface Character {
 
     /**
      * character take off ring accessory
+     * @param ring accessory ring type
      * effects: "You didn't equip ring yet" printed if character didn't wear ring
      * effects: cancel buff effects of ring from total stats of character
      */
@@ -285,6 +299,7 @@ public interface Character {
 
     /**
      * character put on armor accessory
+     * @param armor accessory armor type
      * effects: "you already equip armor" printed if character is already wore armor
      * effects: update buff stats and total stats of character consider to effects of armor
      */
@@ -303,6 +318,7 @@ public interface Character {
 
     /**
      * character take off armor accessory
+     * @param armor accessory armor type
      * effects: "You didn't equip arnor yet" printed if character didn't wear arnor
      * effects: cancel buff effects of arnor from total stats of character
      */
@@ -321,6 +337,7 @@ public interface Character {
 
     /**
      * character take damage
+     * @param damage value of damage did by enemy
      * effects: print out amount of actual damage that character take(input damage can reduce by def of character)
      * effects: decrease currentHp of character by damage taken
      * effects: "your character is unconscious" printed and set isUnconscious = true when currentHp is lower than 1
